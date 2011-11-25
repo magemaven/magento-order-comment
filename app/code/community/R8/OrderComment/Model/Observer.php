83197de7d3ada5a24d5f114f975bdd2d1c7c28dc
Sergey Storchay <r8@r8.com.ua>
2011-11-25 09:08:20 +0200

    Implement observer to save comment

---
+++ app/code/community/R8/OrderComment/Model/Observer.php
@@ -0,0 +1,20 @@
+<?php
+class R8_OrderComment_Model_Observer extends Varien_Object
+{
+    /**
+     * Save comment from agreement form to order
+     *
+     * @param $observer
+     */
+    public function saveOrderComment($observer)
+    {
+        $orderComment = Mage::app()->getRequest()->getPost('ordercomment');
+        if (is_array($orderComment) && isset($orderComment['comment'])) {
+            $comment = trim($orderComment['comment']);
+
+            if (!empty($comment)) {
+                $observer->getEvent()->getOrder()->setCustomerComment($comment);
+            }
+        }
+    }
+}
