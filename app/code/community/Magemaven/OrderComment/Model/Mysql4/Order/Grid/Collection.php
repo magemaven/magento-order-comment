b69dc371120d653217b345d042dc6b1c642fdfaa
Sergey Storchay <r8@r8.com.ua>
2012-02-19 16:17:41 +0200

    Typo fixes

--- app/code/community/Magemaven/OrderComment/Model/Mysql4/Order/Grid/Collection.php
+++ app/code/community/Magemaven/OrderComment/Model/Mysql4/Order/Grid/Collection.php
@@ -7,7 +7,7 @@
  *
  * @category    Magemaven
  * @package     Magemaven_OrderComment
- * @copyright   Copyright (c) 2011 Sergey Storchay <r8@r8.com.ua>
+ * @copyright   Copyright (c) 2011-2012 Sergey Storchay <r8@r8.com.ua>
  * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 class Magemaven_OrderComment_Model_Mysql4_Order_Grid_Collection extends Mage_Sales_Model_Resource_Order_Grid_Collection
