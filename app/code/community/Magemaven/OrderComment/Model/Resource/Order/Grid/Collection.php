f06613948476cbbd285f4255db3cc6fd14db93dc
Sergey Storchay <r8@r8.com.ua>
2013-04-07 09:19:02 +0300

    Remove legacy code

--- app/code/community/Magemaven/OrderComment/Model/Resource/Order/Grid/Collection.php
+++ app/code/community/Magemaven/OrderComment/Model/Resource/Order/Grid/Collection.php
@@ -40,12 +40,3 @@ class Magemaven_OrderComment_Model_Resource_Order_Grid_Collection extends Mage_S
         return $this;
     }
 }
-
-//// Join order comment
-//$this->getSelect()->joinLeft(
-//    array('ordercomment_table' => $this->getTable('sales/order_status_history')),
-//    'main_table.entity_id = ordercomment_table.parent_id AND ordercomment_table.comment IS NOT NULL',
-//    array(
-//        'ordercomment' => 'ordercomment_table.comment',
-//    )
-//)->group('main_table.entity_id');
