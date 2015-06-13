bc7b254291774aac19b565a5d56c483ae87a61f3
Sven Reichel <sven.reichel@hotmail.com>
2015-06-13 12:57:32 +0200

    fixes #24 removed rewrite for Mage_Adminhtml_Block_Sales_Order_Grid

--- app/code/community/Magemaven/OrderComment/Model/Observer.php
+++ app/code/community/Magemaven/OrderComment/Model/Observer.php
@@ -56,4 +56,45 @@ class Magemaven_OrderComment_Model_Observer extends Varien_Object
         }
 
     }
+	
+    /**
+     * Adds column to admin sales order grid
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function appendColumnToSalesOrderGrid(Varien_Event_Observer $observer)
+    {
+		$block = $observer->getBlock();
+		if (!isset($block)) {
+			return $this;
+		}
+
+		if ($block->getType() == 'adminhtml/sales_order_grid') {
+			/* @var $block Mage_Adminhtml_Block_Sales_Order_Grid */
+			$block->addColumnAfter('ordercomment', array(
+				'header'    	=> Mage::helper('ordercomment')->__('Last Comment'),
+				'type'      	=> 'text',
+				'index' 		=> 'customer_note',
+				'filter_index' 	=> 'order.customer_note',
+			), 'status');
+		}
+    }
+	
+	public function beforeCollectionLoad(Varien_Event_Observer $observer)
+    {
+        $collection = $observer->getData('order_grid_collection');
+        if (!isset($collection)) {
+            return;
+        }
+		
+        /**
+         * Mage_Sales_Model_Mysql4_Order_Grid_Collection
+         */
+        if ($collection instanceof Mage_Sales_Model_Mysql4_Order_Grid_Collection) {
+            /* @var $collection Mage_Sales_Model_Mysql4_Order_Grid_Collection */
+			$collection->getSelect()->join(
+				array('order'=> 'sales_flat_order'), 'order.entity_id = main_table.entity_id', array('order.customer_note')
+			);
+        }
+    }
 }
