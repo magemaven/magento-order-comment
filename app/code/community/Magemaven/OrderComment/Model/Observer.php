69b5d259c0c6a05da5a50cd4f117d823c7eb8f83
Sven Reichel <sven.reichel@hotmail.com>
2016-07-04 11:20:46 +0200

    Added missing table prefix

--- app/code/community/Magemaven/OrderComment/Model/Observer.php
+++ app/code/community/Magemaven/OrderComment/Model/Observer.php
@@ -54,9 +54,8 @@ class Magemaven_OrderComment_Model_Observer extends Varien_Object
                 $statusHistory->setIsVisibleOnFront(1);
             }
         }
-
     }
-	
+
     /**
      * Adds column to admin sales order grid
      *
@@ -64,36 +63,37 @@ class Magemaven_OrderComment_Model_Observer extends Varien_Object
      */
     public function appendColumnToSalesOrderGrid(Varien_Event_Observer $observer)
     {
-		$block = $observer->getBlock();
-		if (!isset($block)) {
-			return $this;
-		}
+        $block = $observer->getBlock();
+        if (!isset($block)) {
+            return $this;
+        }
 
-		if ($block->getType() == 'adminhtml/sales_order_grid') {
-			/* @var $block Mage_Adminhtml_Block_Sales_Order_Grid */
-			$block->addColumnAfter('ordercomment', array(
-				'header'    	=> Mage::helper('ordercomment')->__('Last Comment'),
-				'type'      	=> 'text',
-				'index' 		=> 'customer_note',
-				'filter_index' 	=> 'order.customer_note',
-			), 'status');
-		}
+        if ($block->getType() == 'adminhtml/sales_order_grid') {
+            /* @var $block Mage_Adminhtml_Block_Sales_Order_Grid */
+            $block->addColumnAfter('ordercomment', array(
+                'header'        => Mage::helper('ordercomment')->__('Last Comment'),
+                'type'          => 'text',
+                'index'         => 'customer_note',
+                'filter_index'  => 'order.customer_note',
+            ), 'status');
+        }
     }
-	
-	public function beforeCollectionLoad(Varien_Event_Observer $observer)
+    
+    public function beforeCollectionLoad(Varien_Event_Observer $observer)
     {
         $collection = $observer->getData('order_grid_collection');
         if (!isset($collection)) {
             return;
         }
-		
+
         /**
          * Mage_Sales_Model_Mysql4_Order_Grid_Collection (1.5.1.0) || Mage_Sales_Model_Resource_Order_Grid_Collection (1.9.1.0)
          */
         if ($collection instanceof Mage_Sales_Model_Mysql4_Order_Grid_Collection || $collection instanceof Mage_Sales_Model_Resource_Order_Grid_Collection) {
-			$collection->getSelect()->join(
-				array('order'=> 'sales_flat_order'), 'order.entity_id = main_table.entity_id', array('order.customer_note')
-			);
+            $table = Mage::getSingleton('core/resource')->getTableName('sales_flat_order');
+            $collection->getSelect()->join(
+                array('order'=> $table), 'order.entity_id = main_table.entity_id', array('order.customer_note')
+            );
         }
     }
 }
